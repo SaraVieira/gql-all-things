0005e51dd3a627a0835e9653e07d9ac605f19c02
Sara Vieira <hey@iamsaravieira.com>
2019-05-25 12:05:49 +0400

    add new photos

--- code/state.js
+++ code/state.js
@@ -1,5 +1,5 @@
 export const client = new ApolloClient({
-  uri: 'https://figma-graphql.now.sh/graphql',
+  uri: 'https://api-euwest.graphcms.com/v1/cjke2kn7p00ol01d2pinkptdj/master',
   clientState: {
     defaults,
     resolvers
